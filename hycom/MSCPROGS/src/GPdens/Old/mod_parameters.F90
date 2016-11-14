f5a1bdc7b1da4fe5d26940de4bd261d687cf2e9e
Knut Arild Lisæter <knutal@debian>
2016-11-14 14:58:43 +0100

    ...

--- hycom/MSCPROGS/src/GPdens/Old/mod_parameters.F90
+++
@@ -1,6 +0,0 @@
-module mod_parameters
-real, parameter :: onem=9806.
-real, parameter :: onecm=onem/100.
-real, parameter :: undef=-999.
-!real, parameter :: pi=3.14159265358979323846
-end module mod_parameters
