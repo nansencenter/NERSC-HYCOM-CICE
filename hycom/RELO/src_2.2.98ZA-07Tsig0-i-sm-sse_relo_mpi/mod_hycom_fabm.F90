8eb7bf616f9b1098f6baac2675fda808e8969570
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2017-10-24 09:24:06 +0100

    model -> fabm_model

--- hycom/RELO/src_2.2.98ZA-07Tsig0-i-sm-sse_relo_mpi/mod_hycom_fabm.F90
+++ hycom/RELO/src_2.2.98ZA-07Tsig0-i-sm-sse_relo_mpi/mod_hycom_fabm.F90
@@ -18,32 +18,32 @@ contains
 
       integer m,n,ibio
       integer ivar
-      real dy(ii, size(model%state_variables))
+      real dy(ii, size(fabm_model%state_variables))
 
       if     (ibio.lt.0) then !initialize only
         ! Provide extents of the spatial domain (number of layers nz for a 1D column)
-        call fabm_set_domain(model, ii, jj, kk)
+        call fabm_set_domain(fabm_model, ii, jj, kk)
 
         ! Specify vertical index of surface and bottom
-        call model%set_surface_index(1)
-        !call model%set_bottom_index(nz)
+        call fabm_model%set_surface_index(1)
+        !call fabm_model%set_bottom_index(nz)
 
         call update_fabm_data(n)
 
         ! Check whether FABM has all dependencies fulfilled
         ! (i.e., whether all required calls for fabm_link_*_data have been made)
-        call fabm_check_ready(model)
+        call fabm_check_ready(fabm_model)
 
         ! Initialize the tracers
         ! This sets the values of arrays sent to fabm_link_interior_state_data, in this case interior_state.
         do k=1,kk
           do j=1,jj
-              call fabm_initialize_state(model, 1, ii, j, k)
+              call fabm_initialize_state(fabm_model, 1, ii, j, k)
           end do
         end do
         do j=1,jj
-            call fabm_initialize_bottom_state(model, 1, ii, j)
-            call fabm_initialize_surface_state(model, 1, ii, j)
+            call fabm_initialize_bottom_state(fabm_model, 1, ii, j)
+            call fabm_initialize_surface_state(fabm_model, 1, ii, j)
         end do
 
         return
@@ -58,8 +58,8 @@ contains
       ! Compute source terms and update state
       do k=1,kk
         do j=1,jj
-            call fabm_do(model, 1, ii, j, k, dy)
-            do ivar=1,size(model%state_variables)
+            call fabm_do(fabm_model, 1, ii, j, k, dy)
+            do ivar=1,size(fabm_model%state_variables)
                tracer(1:ii, j, k, n, ivar) = tracer(1:ii, j, k, n, ivar) + delt1 * dy(1:ii, ivar)
             end do
         end do
@@ -72,21 +72,21 @@ contains
         integer, intent(in) :: index
 
         ! Send pointers to state variable data to FABM
-        do ivar=1,size(model%state_variables)
+        do ivar=1,size(fabm_model%state_variables)
           call fabm_link_interior_state_data(model, ivar, tracer(:, :, :, index, ivar))
         end do
-        do ivar=1,size(model%bottom_state_variables)
+        do ivar=1,size(fabm_model%bottom_state_variables)
           !call fabm_link_bottom_state_data(model, ivar, ???)
         end do
-        do ivar=1,size(model%surface_state_variables)
+        do ivar=1,size(fabm_model%surface_state_variables)
           !call fabm_link_surface_state_data(model, ivar, ???)
         end do
 
         ! Transfer pointer to environmental data
         ! Do this for all variables on FABM's standard variable list that the model can provide.
         ! For this list, visit http://fabm.net/standard_variables
-        call model%link_interior_data(standard_variables%temperature, temp(:, :, :, index))
-        call model%link_interior_data(standard_variables%practical_salinity, saln(:, :, :, index))
+        call fabm_model%link_interior_data(standard_variables%temperature, temp(:, :, :, index))
+        call fabm_model%link_interior_data(standard_variables%practical_salinity, saln(:, :, :, index))
     end subroutine
 #endif
 end module
