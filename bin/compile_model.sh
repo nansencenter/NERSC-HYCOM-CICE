e8ccae3707f3f542a84ff93b62a570e15ab2e406
Graig Sutherland <graig.sutherland@gmail.com>
2017-12-12 13:19:18 +0100

    added compile features for alvin/elvis

--- bin/compile_model.sh
+++ bin/compile_model.sh
@@ -126,6 +126,14 @@ elif [ "${unamen:0:4}" == "sisu" ] ; then
    SITE="sisu"
    MACROID=$ARCH.$SITE.$compiler
 
+elif [ "${unamen:0:5}" == "alvin" ] ; then
+   SITE="alvin"
+   MACROID=$ARCH.$SITE.$compiler
+
+elif [ "${unamen:0:5}" == "elvis" ] ; then
+   SITE="elvis"
+   MACROID=$ARCH.$SITE.$compiler
+
 # Generic case. SITE is empty
 elif [[ "${ARCH}" == "Linux" ]] ; then
    SITE=""
@@ -176,6 +184,16 @@ elif [ "$SITE" == "sisu" ] ; then
 	fi
         export ESMF_MOD_DIR=${ESMF_DIR}/mod/modO/Unicos.$compiler.64.mpi.default/
 	export ESMF_LIB_DIR=${ESMF_DIR}/lib/libO/Unicos.$compiler.64.mpi.default/
+
+elif [ "$SITE" == "alvin" ] || ["$SITE" == "elvis" ] ; then
+    echo "hardcoded settings for $SITE"
+    if [[ -z "${ESMF_DIR}" ]] ; then
+       export ESMF_DIR=/home/sm_grasu/local
+    fi
+
+   export ESMF_MOD_DIR=${ESMF_DIR}/mod/modO/Linux.$compiler.64.mpi.default/
+   export ESMF_LIB_DIR=${ESMF_DIR}/lib/libO/Linux.$compiler.64.mpi.default/
+
 # If site is not given, try to use a generic setup. Macro names composed of compiler name and mpi lib name (openmpi, mpich, lam, etc etc(
 elif [[ "${unames:0:5}" == "Linux" ]] && [[ "$SITE" == "" ]] ; then
    if [ -z "${ESMF_DIR}" ] ; then
