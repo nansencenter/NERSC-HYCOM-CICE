f40cef9d1693a283532cd05209c035f16b134969
Caglar <vc.yumruktepe@gmail.com>
2020-09-01 17:24:02 +0200

    python script is now generic and a small fix on same bioriver point used multiple times

--- bin/glodapBIO_to_hycom.py
+++ bin/glodapBIO_to_hycom.py
@@ -1,20 +1,23 @@
 import numpy
 import os
 import math
+import argparse
 
-# CAGLAR MAY 2019 ----
-# HOW TO USE:
-# locate the river flux file and assign to "river_volume_flux_file"
-# locate the GLODAPv2 nutrient load file and assign to "river_nutrient_flux_file"
-# save the output file in git folder as " NERSC-HYCOM-CICE/input/biorivers.dat"
-# river scripts look for that exact output location and biorivers.dat
-# works in any directory
-# make sure to adjust input file names twice as they appear in the code twice
-# double check that searchradius does not co-locate nutrient load locations multiple times
-#
-# TO DO:
-# make the script automatically find input and output directories
-# my python knowledge is limited at this point, output formatting could be shortened by some other expert, see below
+'''
+CAGLAR AUG 2020 ----
+HOW TO USE:
+locate the river flux file and assign to "volumefile"
+locate the GLODAPv2 nutrient load file and assign to "nutfile"
+save the output file in git folder as " NERSC-HYCOM-CICE/input/biorivers.dat"
+river scripts look for that exact output location and biorivers.dat
+works in any directory
+
+script execute:
+python glodapBIO_to_hycom.py volumefile nutfile hycompath radius
+
+example:
+python glodapBIO_to_hycom.py /cluster/home/cagyum/DEVEL/NERSC-HYCOM-CICE/input/rivers_ahype-ehype_clim_rev2.dat /cluster/home/cagyum/DEVEL/NERSC-HYCOM-CICE/input/globalNEWS_2012.dat /cluster/home/cagyum/DEVEL/NERSC-HYCOM-CICE/ 20
+'''
 
 def distance_on_unit_sphere(lat1, long1, lat2, long2):
 
@@ -35,15 +38,16 @@ def distance_on_unit_sphere(lat1, long1, lat2, long2):
 
   return arc
 
-searchradius = 20. # kilometers
-river_volume_flux_file   = open("/cluster/home/cagyum/DEVEL/NERSC-HYCOM-CICE/input/rivers_ahype-ehype_clim_rev2.dat")
-river_nutrient_flux_file = open("/cluster/home/cagyum/DEVEL/NERSC-HYCOM-CICE/input/globalNEWS_2012.dat")
+def main(volumefile,nutfile,hycompath,radius):
+   searchradius = radius # kilometers
+   river_volume_flux_file   = open(volumefile) 
+   river_nutrient_flux_file = open(nutfile) 
 
-fflux      = river_volume_flux_file.readlines()
-nfluxlines = len(fflux)/3 # calculates number of rivers in flux file
-river_volume_flux_file   = open("/cluster/home/cagyum/DEVEL/NERSC-HYCOM-CICE/input/rivers_ahype-ehype_clim_rev2.dat") # open again for colocation
+   fflux      = river_volume_flux_file.readlines()
+   nfluxlines = len(fflux)/3 # calculates number of rivers in flux file
+   river_volume_flux_file   = open(volumefile)  # open again for colocation
 
-class DataFlux:
+   class DataFlux:
       name    = ["" for x in range(nfluxlines)]
       onoff   = ["" for x in range(nfluxlines)]
       lat     = numpy.zeros(nfluxlines)
@@ -52,8 +56,8 @@ class DataFlux:
       monthly = numpy.zeros((nfluxlines,12))
 
 
-k=0 # read in flux information
-for line in river_volume_flux_file: 
+   k=0 # read in flux information
+   for line in river_volume_flux_file: 
     if   ( line.split()[0] == 'T' ) or ( line.split()[0] == 'F' ) :
               DataFlux.onoff[k] = line.split()[0]
               DataFlux.name[k] = line.split()[1]
@@ -65,13 +69,13 @@ for line in river_volume_flux_file:
               DataFlux.lat[k] = float( line.split()[0] )
               DataFlux.lon[k] = float( line.split()[1] )
               k=k+1
-river_volume_flux_file.close()
+   river_volume_flux_file.close()
 
 
-fload      = river_nutrient_flux_file.readlines() # open again for colocation
-nloadlines = len(fload)-1
+   fload      = river_nutrient_flux_file.readlines() # open again for colocation
+   nloadlines = len(fload)-1
 
-class DataLoad:
+   class DataLoad:
       name = ["" for x in range(nloadlines)]
       lat  = numpy.zeros(nloadlines)
       lon  = numpy.zeros(nloadlines)
@@ -80,8 +84,8 @@ class DataLoad:
       sil  = numpy.zeros(nloadlines)
       don  = numpy.zeros(nloadlines)
 
-k=0 # read in nutrient information
-with open("/cluster/home/cagyum/DEVEL/NERSC-HYCOM-CICE/input/globalNEWS_2012.dat","r") as river_nutrient_flux_file:
+   k=0 # read in nutrient information
+   with open(nutfile) as river_nutrient_flux_file:
      first_line = river_nutrient_flux_file.readline()
      for line in river_nutrient_flux_file:
          DataLoad.name[k] = line.split()[1]
@@ -93,7 +97,7 @@ with open("/cluster/home/cagyum/DEVEL/NERSC-HYCOM-CICE/input/globalNEWS_2012.dat
          DataLoad.sil[k]  = float( line.split()[30] ) / 1000.
          k=k+1
 
-class Colocated:
+   class Colocated:
       fluxname = ["" for x in range(nfluxlines)]
       loadname = ["" for x in range(nfluxlines)]
       onoff    = ["" for x in range(nfluxlines)]
@@ -107,8 +111,8 @@ class Colocated:
       loadsil  = numpy.zeros(nfluxlines)
       loaddon  = numpy.zeros(nfluxlines)
 
-outfile = open("/cluster/home/cagyum/DEVEL/NERSC-HYCOM-CICE/input/biorivers.dat","w") 
-for x in range(nfluxlines):
+   outfile = open(hycompath + "input/biorivers.dat","w")
+   for x in range(nfluxlines):
       distance = numpy.zeros(nloadlines)
       for y in range(nloadlines):
             distance[y] = distance_on_unit_sphere( DataFlux.lat[x], DataFlux.lon[x], DataLoad.lat[y], DataLoad.lon[y] ) 
@@ -140,6 +144,21 @@ for x in range(nfluxlines):
          Colocated.loadsil[x]  = 0.#-999.
          Colocated.loaddon[x]  = 0.#-999.
 
+   for x in range(nfluxlines):
+      found2 = numpy.where((Colocated.loadlon == Colocated.loadlon[x]) & (Colocated.loadlat == Colocated.loadlat[x]))
+      if (Colocated.loadlon[x] != 0.) & (Colocated.loadlat[x] != 0.) :
+        if numpy.size(found2)>1 : 
+
+           print('found ' + numpy.str(numpy.size(found2)) + ' locations for the same bioriver point @indexes ' + numpy.str(found2))
+
+           nloc = numpy.size(found2)
+           distloc = numpy.zeros((nloc))
+           for dd in range(nloc):
+               distloc[dd] = 1./distance_on_unit_sphere( Colocated.fluxlat[found2[0][dd]], Colocated.fluxlon[found2[0][dd]], Colocated.loadlat[found2[0][dd]], Colocated.loadlon[found2[0][dd]] )
+           Colocated.loadnit[found2[0][found2[0]==x]] = distloc[found2[0]==x]/distloc.min() / ( numpy.sum( distloc/distloc.min() ) ) * Colocated.loadnit[found2[0][found2[0]==x]]
+           Colocated.loadpho[found2[0][found2[0]==x]] = distloc[found2[0]==x]/distloc.min() / ( numpy.sum( distloc/distloc.min() ) ) * Colocated.loadpho[found2[0][found2[0]==x]]
+           Colocated.loadsil[found2[0][found2[0]==x]] = distloc[found2[0]==x]/distloc.min() / ( numpy.sum( distloc/distloc.min() ) ) * Colocated.loadsil[found2[0][found2[0]==x]]
+
       outfile.write('{:3}'.format(Colocated.onoff[x]) + Colocated.fluxname[x]  + os.linesep)
       outfile.write(str( '{:10.1f}'.format(Colocated.fluxann[x])  ) + str( '{:8.5f}'.format(DataFlux.monthly[x][0])  ) + str( '{:8.5f}'.format(DataFlux.monthly[x][1])  ) + str( '{:8.5f}'.format(DataFlux.monthly[x][2])  ) \
                                                                      + str( '{:8.5f}'.format(DataFlux.monthly[x][3])  ) + str( '{:8.5f}'.format(DataFlux.monthly[x][4])  ) + str( '{:8.5f}'.format(DataFlux.monthly[x][5])  ) \
@@ -162,5 +181,14 @@ for x in range(nfluxlines):
                                                                      + str( '{:8.5f}'.format(DataFlux.monthly[x][9])  ) + str( '{:8.5f}'.format(DataFlux.monthly[x][10])  ) + str( '{:8.5f}'.format(DataFlux.monthly[x][11])  ) \
                                                                      + os.linesep)
       outfile.write( str( '{:9.2f}'.format(Colocated.fluxlat[x])  ) + str( '{:9.2f}'.format(Colocated.fluxlon[x]) ) + os.linesep)
-outfile.close()
+   outfile.close()
 
+if __name__ == "__main__" :
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('volumefile',  type=str)
+    parser.add_argument('nutfile',  type=str)
+    parser.add_argument('hycompath',  type=str)
+    parser.add_argument('radius',  type=float)
+    args = parser.parse_args()
+
+    main(args.volumefile,args.nutfile,args.hycompath,args.radius)
