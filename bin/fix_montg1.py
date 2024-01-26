a4a61c5d9cf9b6b0308aa9a6d315e02ad7de078f
Shuang Gao <sgao@login-2.betzy.sigma2.no>
2024-01-26 15:10:05 +0100

    Update nesting routines * Update nesting documentation * Modify 'archvz2hycom_biophys.sh' in order to be able to run in parallel * Update bio-nesting from weekly to daily * Modify the way of writing headers in 'calc_montg1.py' and 'fix_montg1.py'

--- bin/fix_montg1.py
+++ bin/fix_montg1.py
@@ -34,12 +34,9 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 logger.propagate=False
 
-cline1="HYCOM nested archive files"
-cline2="NEMO fields on hycom grid with corrected montg1 variable"
-cline3="Expt 01.1  nhybrd=50 nsigma= 0 ds00= 1.00 dp00= 1.00 dp00x= 450.0 dp00f=1.150"
 
-
-def main(archv_files,regress_file,opath,depth_file,header_line1=cline1,header_line2=cline2,header_line3=cline3 ) :
+def main(archv_files,regress_file,opath,depth_file) :
+#,header_line1=None,header_line2=None,header_line3=None ) :
 
    regr = open(regress_file,'rb')
    slope,intercept,nemomeandt = pickle.load(regr,encoding="latin1")
@@ -61,7 +58,9 @@ def main(archv_files,regress_file,opath,depth_file,header_line1=cline1,header_li
 
       logger.debug("Processing %s"%(archv_file))
       arcfile=abfile.ABFileArchv(archv_file,"r")
-      
+      header_line1=arcfile._header[0]
+      header_line2=arcfile._header[1]
+      header_line3=arcfile._header[2]
       srfhgt=arcfile.read_field("srfhgt",0)
       montg1=arcfile.read_field("montg1",0)
       montg1=(srfhgt-nemomeandt) * slope + intercept
@@ -74,7 +73,7 @@ def main(archv_files,regress_file,opath,depth_file,header_line1=cline1,header_li
       print(dummy.max())
 #      logger.info("Estimated montg1 ")
       fnameout = opath+str(archv_file[-19:])
-      arcfile_out=abfile.ABFileArchv(fnameout,"w",iversn=iversn,yrflag=yrflag,iexpt=iexpt,mask=False,cline1=header_line1,cline2=header_line2,cline3=header_line3)
+      arcfile_out=abfile.ABFileArchv(fnameout,"w",iversn=iversn,yrflag=yrflag,iexpt=iexpt,idm=idm,jdm=jdm,mask=False,cline1=header_line1,cline2=header_line2,cline3=header_line3)
 
       for keys in sorted( arcfile.fields.keys() ) :
           fieldname = arcfile.fields[keys]["field"]
@@ -103,14 +102,15 @@ def main(archv_files,regress_file,opath,depth_file,header_line1=cline1,header_li
 if __name__ == "__main__" :
 
    parser = argparse.ArgumentParser(description='Calculate montgomery potential in 1st layer (used in archv files)')
-   parser.add_argument('--header_line1',          type=str, help="First header line, applicable only for archm & archv",default=cline1)
-   parser.add_argument('--header_line2',          type=str, help="Montgomery with or without barotropic pressure",default=cline2)
-   parser.add_argument('--header_line3',          type=str, help="Montgomery with or without barotropic pressure",default=cline3)
+   #parser.add_argument('--header_line1',          type=str, help="First header line, applicable only for archm & archv",default=None)
+   #parser.add_argument('--header_line2',          type=str, help="Montgomery with or without barotropic pressure",default=None)
+   #parser.add_argument('--header_line3',          type=str, help="Montgomery with or without barotropic pressure",default=None)
 
    parser.add_argument('archive_file',     type=str, help='Files to add montg1 to',nargs="+")
    parser.add_argument('opath',     type=str, help='Output files path')
    parser.add_argument('regress_file', type=str, help='regression file path')
    parser.add_argument('depth_file', type=str, help='path to regional_depth.a file')
    args = parser.parse_args()
-   main(args.archive_file,args.opath,args.regress_file,args.depth_file,header_line1=args.header_line1,header_line2=args.header_line2,header_line3=args.header_line3)
+   main(args.archive_file,args.opath,args.regress_file,args.depth_file)
+#,header_line1=args.header_line1,header_line2=args.header_line2,header_line3=args.header_line3)
 
