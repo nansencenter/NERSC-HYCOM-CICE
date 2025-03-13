ecca0798ddce8a0656958087223b9138c8f39241
Alfatih Ali <alfatiha@met.no>
2025-03-13 13:47:15 +0000

    add implementation to allow for read and use of high frequency river forcing via  new hycom_nml keyword: highfq_river

--- hycom/RELO/src_2.2.98ZA-07Tsig0-i-sm-sse_relo_mpi/mod_NERSCnml.F90
+++ hycom/RELO/src_2.2.98ZA-07Tsig0-i-sm-sse_relo_mpi/mod_NERSCnml.F90
@@ -8,7 +8,9 @@ module mod_NERSCnml
  
   logical,save, public :: &
     write_arche, &      ! print arche files or not
-    sss_underice        ! relaxtion under ice (false if no) 
+    sss_underice, &      ! relaxtion under ice (false if no) 
+    highfq_river        ! high frequency rivers (false if no) 
+
 
   public NERSC_init
 
@@ -20,10 +22,11 @@ module mod_NERSCnml
     implicit none
     integer (4), parameter :: funi=503
     integer (4) :: nml_err
-    namelist /hycom_nml/ write_arche,sss_underice
+    namelist /hycom_nml/ write_arche,sss_underice,highfq_river
     !default values
     write_arche     = .false.
     sss_underice    = .false.
+    highfq_river    = .false.
     ! read namelist
     open (funi, file='../hycom_opt', status='old',iostat=nml_err)
     if (nml_err .ne. 0) then
@@ -50,6 +53,7 @@ module mod_NERSCnml
       write (lp,*)'NERSC HYCOM: Reading hycom_nml from: ../hycom_opt'
       write (lp,*)'NERSC HYCOM: Write arche    = ',write_arche
       write (lp,*)'      HYCOM: sss_underice   = ',sss_underice
+      write (lp,*)'      HYCOM: highfq_river   = ',highfq_river
     endif !1st tile
     call xcsync(flush_lp)
 
