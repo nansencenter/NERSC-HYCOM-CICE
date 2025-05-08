2b9a1933f15458a64b9448bb71fdd5de7209aa9c
Shuang Gao <sgao@login-2.betzy>
2025-05-08 14:23:40 +0200

    starting point of implementing ice-algae drift

--- hycom/RELO/src_2.2.98ZA-07Tsig0-i-sm-sse_relo_mpi/mod_hycom_fabm.F90
+++ hycom/RELO/src_2.2.98ZA-07Tsig0-i-sm-sse_relo_mpi/mod_hycom_fabm.F90
@@ -55,7 +55,8 @@ module mod_hycom_fabm
    real :: wndstr,strspd
    real, allocatable :: h(:, :,:),delZ(:),codepth(:,:,:),cotemp(:,:,:),cosal(:,:,:),codens(:,:,:)
    real, allocatable :: hriver(:, :)
-   real, allocatable :: coice_conc(:, :)
+!  these are used for ice algae module
+   real, allocatable :: coice_conc(:,:),coice_thickness(:,:),  codh_growth(:,:) !,cosnow_thickness(:,:) 
    real, allocatable, target :: fabm_surface_state(:, :, :, :)
    real, allocatable, target :: fabm_bottom_state(:, :, :, :)
    real, allocatable :: fabm_surface_state_old(:, :, :)
@@ -193,8 +194,10 @@ contains
         allocate(cotemp(ii,jj,kk))
         allocate(cosal(ii,jj,kk))
         allocate(codens(ii,jj,kk))
-        allocate(coice_conc(ii,jj))
         allocate(hriver(ii, jj))
+        allocate(coice_conc(ii, jj))
+        allocate(coice_thickness(ii, jj))
+        allocate(codh_growth(ii, jj))
         allocate(fabm_surface_state(1-nbdy:idm+nbdy, 1-nbdy:jdm+nbdy, 2, size(fabm_model%surface_state_variables)))
         allocate(fabm_bottom_state(1-nbdy:idm+nbdy, 1-nbdy:jdm+nbdy, 2, size(fabm_model%bottom_state_variables)))
         allocate(fabm_surface_state_old(1-nbdy:idm+nbdy, 1-nbdy:jdm+nbdy, size(fabm_model%surface_state_variables)))
@@ -228,6 +231,14 @@ contains
         call fabm_model%link_horizontal_data(fabm_standard_variables%mole_fraction_of_carbon_dioxide_in_air,atmco2_fabm(1:ii,1:jj))
         call fabm_model%link_horizontal_data(fabm_standard_variables%bottom_stress, bottom_stress(1:ii, 1:jj))
         call fabm_model%link_scalar(type_global_standard_variable(name='time_step', units='s'), delt1)
+
+        call fabm_model%link_interior_data(fabm_standard_variables%temperature,cotemp(1:ii,1:jj, 1:kk))
+        call fabm_model%link_interior_data(fabm_standard_variables%practical_salinity,cosal(1:ii,1:jj,1:kk))
+        call fabm_model%link_interior_data(fabm_standard_variables%density,codens(1:ii,1:jj, 1:kk))
+        call fabm_model%link_interior_data(fabm_standard_variables%pressure,codepth(1:ii,1:jj, 1:kk))
+        call fabm_model%link_horizontal_data(fabm_standard_variables%ice_conc,coice_conc(1:ii,1:jj))
+        call fabm_model%link_horizontal_data(fabm_standard_variables%ice_thickness,coice_thickness(1:ii,1:jj))
+        call fabm_model%link_horizontal_data(fabm_standard_variables%dh_growth,codh_growth(1:ii,1:jj))
 
         call update_fabm_data(1, initializing=.true.)  ! initialize the entire column of wet points, including thin layers
 
@@ -982,7 +993,7 @@ call fabm_model%finalize_outputs
         ! copying bottom value for pelagic tracers to all layers below bottom.
 
         do ivar=1,size(fabm_model%interior_state_variables)
-          if ( fabm_model%interior_state_variables(ivar)%name == "ECO_det" ) then
+          if ( fabm_model%interior_state_variables(ivar)%name == "ECO_det2" ) then
              indDET = ivar
           end if
           if ( fabm_model%interior_state_variables(ivar)%name == "ECO_dsnk" ) then
@@ -1053,7 +1064,6 @@ call fabm_model%finalize_outputs
       do ivar=1,size(fabm_model%interior_state_variables)
         if (.not.all(ieee_is_finite(tracer(1:ii, 1:jj, 1:kk, index, ivar)))) then
           write (*,*) location, 'Interior state variable not finite:', ivar,'range', minval(tracer(1:ii, 1:jj, 1:kk, index, ivar)), maxval(tracer(1:ii,1:jj, 1:kk, index, ivar)),fabm_model%interior_state_variables(ivar)%name
-          
         end if
       end do
 
@@ -1225,6 +1235,7 @@ call fabm_model%finalize_outputs
         integer :: i, j, k
         integer :: ivar
         real, parameter :: rho_0 = 1025.   ! [kg/m3]
+        logical, save :: lcoice_thickness_initialized = .false. 
         ! Update cell thicknesses (m)
         h(:, :, :) = dp(1:ii, 1:jj, 1:kk, index)/onem
 
@@ -1237,7 +1248,6 @@ call fabm_model%finalize_outputs
         if (.not.initializing) then
           !call fabm_update_time(fabm_model, real(nstep))
           call fabm_gettime() 
-
 
           ! Update surface forcing and internal variables for biology 
           do j=1,jj
@@ -1285,8 +1295,15 @@ call fabm_model%finalize_outputs
                        atmco2_2 = atmco2_1 / 9.81 * 10.**(-4.0)
                        atmco2_3 = pair / 9.81 * 10.**(-2.0)
                        atmco2_fabm(i,j) = atmco2_0 * (atmco2_3 - atmco2_2) * 0.997                     
+		       ! for ice-algae
+		       coice_conc(i,j) = covice(i,j)
+                       if (lcoice_thickness_initialized) then
+                         codh_growth(i,j) = (thkice(i,j)*coice_conc(i,j) - coice_thickness(i,j)) / delt1 
+                       else 
+                         codh_growth(i,j) = 0.
+                       end if 
+                       coice_thickness(i,j) = thkice(i,j)*coice_conc(i,j)
 
-                       coice_conc(i,j)=covice(i,j)
                        do k=1,kk
                           delZ(k) = dp(i,j,k,index)/onem                    !
                           if(k.eq.1)then                                    !
@@ -1295,22 +1312,23 @@ call fabm_model%finalize_outputs
                              codepth(i,j,k) = codepth(i,j,k-1)+delZ(k)      ! water depth
                           end if                                            !
                           cotemp(i,j,k) = max(-3.999,temp(i, j, k, index))  ! water temparature
-                          cosal(i,j,k)  = max(5.0,saln(i, j, k, index))     ! salinity
+                          cosal(i,j,k)  = max(5.0,saln(i, j, k, index))     ! salinity        
                           codens(i,j,k) = th3d(i, j, k, index)+thbase+1000. ! water density
-
                        end do
     !              end if
               end do
           end do
+          lcoice_thickness_initialized = .true.
         end if
         ! Transfer pointer to environmental data
         ! Do this for all variables on FABM's standard variable list that the model can provide.
         ! For this list, visit http://fabm.net/standard_variables
-        call fabm_model%link_interior_data(fabm_standard_variables%temperature,cotemp(1:ii,1:jj, 1:kk))
-        call fabm_model%link_interior_data(fabm_standard_variables%practical_salinity,cosal(1:ii,1:jj,1:kk))
-        call fabm_model%link_interior_data(fabm_standard_variables%density,codens(1:ii,1:jj, 1:kk))
-        call fabm_model%link_interior_data(fabm_standard_variables%pressure,codepth(1:ii,1:jj, 1:kk))
-        call fabm_model%link_horizontal_data(fabm_standard_variables%ice_area_fraction, coice_conc(1:ii, 1:jj))
+        !call fabm_model%link_interior_data(fabm_standard_variables%temperature,cotemp(1:ii,1:jj, 1:kk))
+        !call fabm_model%link_interior_data(fabm_standard_variables%practical_salinity,cosal(1:ii,1:jj,1:kk))
+        !call fabm_model%link_interior_data(fabm_standard_variables%density,codens(1:ii,1:jj, 1:kk))
+        !call fabm_model%link_interior_data(fabm_standard_variables%pressure,codepth(1:ii,1:jj, 1:kk))
+        !call fabm_model%link_horizontal_data(fabm_standard_variables%ice_conc,coice_conc(1:ii,1:jj))
+        !call fabm_model%link_horizontal_data(fabm_standard_variables%ice_thickness,coice_thickness(1:ii,1:jj))
 
         call update_fabm_state(index)
     end subroutine update_fabm_data
