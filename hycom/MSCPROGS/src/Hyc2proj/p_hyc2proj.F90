ba2a11ea3354c6492ed786fb42f31b71ff2ec25c
Çağlar Yumruktepe <vc.yumruktepe@gmail.com>
2025-05-26 11:47:20 +0200

    added/corrected NECCTON variables to hyc2proj (#284)

--- hycom/MSCPROGS/src/Hyc2proj/p_hyc2proj.F90
+++ hycom/MSCPROGS/src/Hyc2proj/p_hyc2proj.F90
@@ -72,7 +72,7 @@ program p_hyc2proj
 !AS06092011 - adding biological variables for MyOcean
    real, allocatable, dimension(:,:,:) :: fla, dia, nit, pho, oxy, pp, biovar,s1000,ccl
    real, allocatable, dimension(:,:,:) :: u,v
-   real, allocatable, dimension(:,:,:) :: micro, meso, sil,dic,ph,det, pco2,dsnk,dom
+   real, allocatable, dimension(:,:,:) :: micro, meso, sil,dic,ph,det,detr, pco2,dsnk,dom
    real, allocatable, dimension(:,:)   :: biovar2d
    real, allocatable, dimension(:,:)   :: hy2d, hy2d2, regu2d, strmf, &
       mld1, mld2, dplayer, meanssh, sla, ub, vb, mqlon, mqlat
@@ -590,23 +590,21 @@ program p_hyc2proj
                   hy3d=biovar
                   deallocate(meso,biovar)
                else if (trim(fld(ifld)%fextract)=='poc') then
-                  ! Compute POC biomass (mmole C m-3)
+                  ! Compute total POC biomass (mole C m-3)
                   allocate(micro(idm,jdm,kdm))
-                  allocate(meso(idm,jdm,kdm))
                   allocate(dia(idm,jdm,kdm))
                   allocate(fla(idm,jdm,kdm))
                   allocate(ccl(idm,jdm,kdm))
                   allocate(det(idm,jdm,kdm))
                   allocate(biovar(idm,jdm,kdm))
                   call HFReadField3D(hfile,micro,idm,jdm,kdm,'ECO_micr   ',1)
-                  call HFReadField3D(hfile,meso,idm,jdm,kdm,'ECO_meso    ',1)
                   call HFReadField3D(hfile,dia,idm,jdm,kdm,'ECO_dia    ',1)
                   call HFReadField3D(hfile,fla,idm,jdm,kdm,'ECO_fla    ',1)
                   call HFReadField3D(hfile,ccl,idm,jdm,kdm,'ECO_ccl    ',1)
                   call HFReadField3D(hfile,det,idm,jdm,kdm,'ECO_det    ',1)                                                      
-                  call poc(micro,meso,dia,fla,ccl,det,biovar,idm,jdm,kdm)
+                  call poc(micro,dia,fla,ccl,det,biovar,idm,jdm,kdm)
                   hy3d=biovar
-                  deallocate(micro,meso,dia,fla,ccl,det,biovar)
+                  deallocate(micro,dia,fla,ccl,det,biovar)
                else if (trim(fld(ifld)%fextract)=='doc') then
                   ! Compute DOC biomass (mmole C m-3)
                   allocate(dom(idm,jdm,kdm))
@@ -615,6 +613,14 @@ program p_hyc2proj
                   call docc(dom,biovar,idm,jdm,kdm)
                   hy3d=biovar
                   deallocate(dom,biovar)
+               else if (trim(fld(ifld)%fextract)=='det') then
+                  ! Compute detritus biomass (mol C m-3)
+                  allocate(detr(idm,jdm,kdm))
+                  allocate(biovar(idm,jdm,kdm))
+                  call HFReadField3D(hfile,detr,idm,jdm,kdm,'ECO_det     ',1)
+                  call detc(detr,biovar,idm,jdm,kdm)
+                  hy3d=biovar
+                  deallocate(detr,biovar)
                else if (trim(fld(ifld)%fextract)=='oxygen') then
                   ! Compute dissolved oxygen (mmole m-3)
                   allocate(oxy(idm,jdm,kdm))
