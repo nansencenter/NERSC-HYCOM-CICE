0352015640a1c824f95f3343d232b66a3516182c
TillRasmussen <tillrasmussen@gmail.com>
2025-09-01 11:27:20 +0200

    Updates to hycom_opt. Now it has to be in SCRATCH when it is run

--- hycom/RELO/src_2.2.98ZA-07Tsig0-i-sm-sse_relo_mpi/mod_NERSCnml.F90
+++ hycom/RELO/src_2.2.98ZA-07Tsig0-i-sm-sse_relo_mpi/mod_NERSCnml.F90
@@ -34,7 +34,7 @@ module mod_NERSCnml
     if (nml_err .ne. 0) then
       if  (mnproc.eq.1) then
         write (lp,'(a)') &
-          'NERSC HYCOM ERROR: WARNING: hycom_nml namelist not read from file: ../hycom_opt'
+          'NERSC HYCOM ERROR: WARNING: hycom_nml namelist not read from file: ./hycom_opt'
         call flush(lp)
       endif
     endif
@@ -52,9 +52,9 @@ module mod_NERSCnml
     end do
     close(funi)
     if (mnproc.eq.1) then
-      write (lp,*)'NERSC HYCOM: Reading hycom_nml from: ../hycom_opt'
+      write (lp,*)'NERSC HYCOM: Reading hycom_nml from: ./hycom_opt'
       write (lp,*)'NERSC HYCOM: Write arche    = ',write_arche
-      write (lp,*)'      HYCOM: sss_underice   = ',sss_underice
+      write (lp,*)'NERSC HYCOM: sss_underice   = ',sss_underice
     endif !1st tile
     call xcsync(flush_lp)
 
