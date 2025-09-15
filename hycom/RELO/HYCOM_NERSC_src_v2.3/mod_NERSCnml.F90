d48fd193eaf5e97d09587124657a312235aa67c2
Alfatih Ali <alfatiha@met.no>
2025-09-15 10:40:14 +0200

    Update mod_NERSCnml.F90
    removed blank line

--- hycom/RELO/HYCOM_NERSC_src_v2.3/mod_NERSCnml.F90
+++ hycom/RELO/HYCOM_NERSC_src_v2.3/mod_NERSCnml.F90
@@ -24,7 +24,6 @@ module mod_NERSCnml
     implicit none
     integer (4), parameter :: funi=503
     integer (4) :: nml_err
-    
     namelist /hycom_nml/ write_arche,sss_underice,highfq_river,sssrmx_scalar
     !default values
     write_arche     = .false.
