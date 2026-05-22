8fcc5370eafd151f01be567ab0ad2c43ca458544
Nora Loose <nora.loose@gmail.com>
2026-05-22 10:25:20 +0200

    Revert changes in nemo2archvz_regular

--- bin/nemo2archvz_regular.py
+++ bin/nemo2archvz_regular.py
@@ -506,13 +506,13 @@ def p2v_2d(var_p) :
     var_u[:,0] = 2.0*var_u[:,1] - var_u[:,2]
     return var_u
 
-def read_grid(filemesh, coord_file) :
-
-    ncid0=netCDF4.Dataset(coord_file,"r")
+def read_grid(filemesh) :
+    
+    ncid0=netCDF4.Dataset(filemesh[:-13]+"coordinates.nc","r")
     numpy.seterr(invalid='ignore')
     e3t=ncid0.variables["e3t"][:]
     ncid0.close()
-
+    
     ncid0=netCDF4.Dataset(filemesh,"r")
     lon=ncid0.variables["longitude"][:]
     lat=ncid0.variables["latitude"][:]
@@ -538,8 +538,8 @@ def read_grid(filemesh, coord_file) :
     return hdept,gdept,mbathy,mbathy_u,mbathy_v,mbathyfill,mask,e3t,plon,plat
 
 
-def main(meshfile,file,iexpt=10,iversn=22,yrflag=3,bio_file=None,coord_file=None) :
-
+def main(meshfile,file,iexpt=10,iversn=22,yrflag=3,bio_file=None) :
+    
     #
     # Trim input netcdf file name being appropriate for reading
     #
@@ -550,7 +550,7 @@ def main(meshfile,file,iexpt=10,iversn=22,yrflag=3,bio_file=None,coord_file=None
     # Note that for now, we are using T-grid in vertical which may need
     # to be improved by utilizing W-point along the vertical axis.
     #
-    hdept,gdept,mbathy,mbathy_u,mbathy_v,mbathyfill,mask,e3t,plon,plat=read_grid(meshfile,coord_file=coord_file)
+    hdept,gdept,mbathy,mbathy_u,mbathy_v,mbathyfill,mask,e3t,plon,plat=read_grid(meshfile)
     logger.warning("Reading grid information from regional.grid.[ab] (not completed)")
     #
     # Convert from P-point (i.e. NEMO grid) to U and V HYCOM grids
@@ -936,8 +936,6 @@ if __name__ == "__main__" :
     parser.add_argument('--iexpt',    type=int,default=10,  help="    ")
     parser.add_argument('--iversn',   type=int,default=22,  help="    ")
     parser.add_argument('--yrflag',   type=int,default=3,   help="    ")
-    parser.add_argument('--bio_file',   type=str,             help="    ")
-    parser.add_argument('--coord_file', type=str, required=True,
-                        help="Path to coordinates file containing longitude, latitude, and e3t.")
+    parser.add_argument('--bio_file', type=str,             help="    ")
     args = parser.parse_args()
-    main(args.meshfile,args.file,iexpt=args.iexpt,iversn=args.iversn,yrflag=args.yrflag,bio_file=args.bio_file,coord_file=args.coord_file)
+    main(args.meshfile,args.file,iexpt=args.iexpt,iversn=args.iversn,yrflag=args.yrflag,bio_file=args.bio_file)
