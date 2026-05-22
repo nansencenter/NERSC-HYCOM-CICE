2ebd1d79d946dbfad6e3ee3b01582e1c6253d63a
Nora Loose <nora.loose@gmail.com>
2026-05-22 13:17:34 +0200

    Fix nemo2archvz_regular.py

--- bin/nemo2archvz_regular.py
+++ bin/nemo2archvz_regular.py
@@ -700,7 +700,7 @@ def main(meshfile,file,iexpt=10,iversn=22,yrflag=3,bio_file=None,coord_file=None
 #        the data file saved for latitude larger than 30. In the case you change your data file coordinate
 #        configuration you need to modify the following lines
        # Derive bottom level index from the bio data mask (no coord file needed)
-       depth_lev = numpy.sum(numpy.isfinite(no3) & (numpy.abs(no3) < 1e10), axis=0)
+       depth_lev = numpy.sum(numpy.isfinite(no3[:,:,:nx]) & (numpy.abs(no3[:,:,:nx]) < 1e10), axis=0)
 #
 #
 #
