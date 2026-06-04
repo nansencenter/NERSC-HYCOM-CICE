fdad669606e6776ff106a1e3e1f1f7828dea337f
Nora Loose <nora.loose@gmail.com>
2026-06-04 15:22:30 +0200

    Fix path to expt_postprocess.sh

--- TP2a0.10/expt_01.0/srjob.sh
+++ TP2a0.10/expt_01.0/srjob.sh
@@ -52,7 +52,7 @@ srun -n $NMPI --cpu_bind=cores ./hycom_cice
 
 # Cleanup and move data files to data directory - must be in "expt_XXX" dir for this script
 cd $P     ||  { echo "Could not go to dir $P  "; exit 1; }
-../expt_postprocess.sh 
+../bin/expt_postprocess.sh 
 
 exit $?
 
