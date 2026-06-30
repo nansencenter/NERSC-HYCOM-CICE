8f9578a526343871f98acbb84804f77e84c1e496
Lingling Suo <llsuo@login-2.betzy>
2026-06-30 16:21:29 +0200

    revise river_synoptic.sh to read regional.depth.a as the depth file

--- bin/river_synoptic.sh
+++ bin/river_synoptic.sh
@@ -47,7 +47,10 @@ cd       $S || { echo " Could not descend scratch dir $S" ; exit 1;}
 
 ml load matplotlib/3.5.2-foss-2022a
 cd ${BINDIR}/river-topaz/scripts/GloFAS
-depthfile=$(ls ${BASEDIR}/topo/depth_*.a 2>/dev/null | head -1)
+
+depthfile=${BASEDIR}/topo/regional.depth.a
+[ ! -e "$depthfile" ] && { echo "Could not find $depthfile" ; exit 1 ; }
+
 [ -z "$depthfile" ] && { echo "Could not find depth file in ${BASEDIR}/topo/" ; exit 1 ; }
 cmd="python ${BINDIR}/river-topaz/scripts/GloFAS/hycom_river_hifre.py $start $stop $S/ $depthfile"
 eval $cmd   ||  { echo "Error running $cmd " ; exit 1 ; }
