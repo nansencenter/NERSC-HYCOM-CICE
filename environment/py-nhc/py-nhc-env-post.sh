a8a871cac4b90285fb65f99d697d20e0b3656768
Nora Loose <nora.loose@gmail.com>
2026-07-02 11:54:48 +0200

    Remove outdated python envs

--- environment/py-nhc/py-nhc-env-post.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# NB: Set this path!
-export PYLIBSPATH=/path/to/NERSC-HYCOM-CICE/pythonlibs
-
-pip install $PYLIBSPATH/modeltools
-pip install $PYLIBSPATH/modelgrid
-pip install $PYLIBSPATH/gridxsec
-pip install $PYLIBSPATH/abfile
