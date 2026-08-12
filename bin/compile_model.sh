3761f83c6aefe3cad889e803a3dbafecea05bfe1
Nora Loose <nora.loose@gmail.com>
2026-08-12 12:19:20 +0200

    Only keep the tested bits

--- bin/compile_model.sh
+++ bin/compile_model.sh
@@ -44,14 +44,13 @@ usage="
    code in $sourcedir, then pushed to the code repository
 
    Example:
-      $(basename $0) [ -u ] [ -c cluster ] [ -m mpi_library ] compiler
+      $(basename $0) [ -u ]  [ -m mpi_library ]  compiler
 
    arguments   :
-      compiler : compiler to use for. Currently supported: ifort ifx gfortran pgi
+      compiler : compiler to use for. Currently supported: ifort gfortran pgi
 
    optional arguments :
       -u              : update code in build dir from $sourcedir
-      -c cluster      : set cluster/site explicitly (overrides hostname/domain detection)
       -m mpi_library  : on some machines you need to specify what mpi library to use
 
 
@@ -66,34 +65,29 @@ usage="
 "
 
 # This will process optional arguments
-options=$(getopt -o c:m:u -- "$@")
+options=$(getopt -o m:u -- "$@")
 [ $? -eq 0 ] || {
     echo "$usage"
     echo "Error: Incorrect options provided"
     exit 1
 }
 mpilib=""
-cluster=""
 eval set -- "$options"
 while true; do
     case "$1" in
-            -c)
-                  shift;
-                  cluster=$1
-                  ;;
-            -m)
-                  shift;
-                  mpilib=$1
-                  ;;
-            -u)
-                  update="update"
-                  ;;
-            --)
-                  shift
-                  break
-                  ;;
-      esac
-      shift
+    -m)
+       shift;
+       mpilib=$1
+        ;;
+    -u)
+       update="update"
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+    shift
 done
 
 
@@ -102,12 +96,11 @@ if [ $# -gt 0 ] ; then
    compiler=$1
 else 
    echo "$usage"
-   echo "Error: Need to provide compiler to script, options: gfortran pgi ifort ifx "
+   echo "Error: Need to provide compiler to script, options: gfortran pgi ifort "
    exit 1
 fi
 echo "$(basename $0) : compiler=$compiler"
 echo "$(basename $0) : mpilib=$mpilib"
-echo "$(basename $0) : cluster=$cluster"
 
 # Check ARCH based on uname. Only Linux accepted 
 ARCH=$(uname -s)
@@ -128,51 +121,41 @@ unamen=$(uname -n)
 hostnamed=$(hostname -d)
 
 echo $unamen
-if [ -n "${cluster}" ] ; then
-   SITE="${cluster}"
+# Hardcoded cases - hexagon
+if [ "${unamen:5:5}" == "bullx" ] ; then
+   SITE="surfsara"
    MACROID=$ARCH.$SITE.$compiler
-else
-   # Hardcoded cases - hexagon
-   if [ "${unamen:5:5}" == "bullx" ] ; then
-      SITE="surfsara"
-      MACROID=$ARCH.$SITE.$compiler
 
-   elif [ "${unamen:0:5}" == "alvin" ] ; then
-      SITE="alvin"
-      MACROID=$ARCH.$SITE.$compiler
+elif [ "${unamen:0:5}" == "alvin" ] ; then
+   SITE="alvin"
+   MACROID=$ARCH.$SITE.$compiler
 
-   elif [ "${unamen:0:5}" == "elvis" ] ; then
-      SITE="elvis"
-      MACROID=$ARCH.$SITE.$compiler
-   elif [ "${hostnamed:0:5}" == "betzy" ] ; then
-      SITE="betzy"
-      MACROID=$ARCH.$SITE.$compiler
-   elif [ "${hostnamed:0:4}" == "fram" ] ; then # fram
-      SITE="fram"
-      MACROID=$ARCH.$SITE.$compiler
-   # Generic case. SITE is empty
-   elif [[ "${ARCH}" == "Linux" ]] ; then
-      if [ -z "${mpilib}" ] ; then
-         echo "mpilib must be set on input running on generic linux machine (-m option)"
-         exit 4
-       fi
-      if [ "${mpilib}" == "olivia" ] ; then
-         SITE="olivia"
-         MACROID=$ARCH.$SITE.$compiler
-      else
-         SITE=""
-         MACROID=$ARCH.$compiler.$mpilib
-      fi
-
-      echo 'MACROID=' ${MACROID}
-       if [ "${mpilib}" == "fram" ]; then
-          SITE="fram"
-       fi
-
-   else
-      echo "Unknown SITE. uname -n gives $unamen"
-      exit 3
+elif [ "${unamen:0:5}" == "elvis" ] ; then
+   SITE="elvis"
+   MACROID=$ARCH.$SITE.$compiler
+elif [ "${hostnamed:0:5}" == "betzy" ] ; then
+   SITE="betzy"
+   MACROID=$ARCH.$SITE.$compiler
+elif [ "${hostnamed:0:4}" == "fram" ] ; then # fram
+   SITE="fram"
+   MACROID=$ARCH.$SITE.$compiler
+# Generic case. SITE is empty
+elif [[ "${ARCH}" == "Linux" ]] ; then
+   SITE=""
+   if [ -z "${mpilib}" ] ; then
+      echo "mpilib must be set on input running on generic linux machine (-m option)"
+      exit 4
    fi
+   MACROID=$ARCH.$compiler.$mpilib
+
+   echo 'MACROID=' ${MACROID}
+   if [ "${mpilib}" == "fram" ]; then
+      SITE="fram"
+   fi
+
+else
+   echo "Unknown SITE. uname -n gives $unamen"
+   exit 3
 fi
 echo "$(basename $0) : SITE=$SITE"
 echo $MACROID
@@ -197,7 +180,7 @@ elif [ "$SITE" == "fram" ] ; then
    export ESMF_MOD_DIR=${ESMF_DIR}mod/
    export ESMF_LIB_DIR=${ESMF_DIR}lib/
 
-elif [ "$SITE" == "betzy" ] || [ "$SITE" == "olivia" ] ; then
+elif [ "$SITE" == "betzy" ] ; then
    export ESMF_DIR=${EBROOTESMF}/
    export ESMF_MOD_DIR=${ESMF_DIR}mod/
    export ESMF_LIB_DIR=${ESMF_DIR}lib/
@@ -347,11 +330,7 @@ if [ ${ICEFLG} -eq 2 ] ; then
    echo $MACROID
    # 1) Compile CICE. Environment variables need to be passe to script
    cd $targetcicedir
-    if [ "$SITE" == "olivia" ] ; then
-       env RES=gx3 GRID=${IDM}x${JDM} SITE=$SITE MACROID=$MACROID CC=icx ./comp_ice.esmf
-    else
-       env RES=gx3 GRID=${IDM}x${JDM} SITE=$SITE MACROID=$MACROID ./comp_ice.esmf
-    fi
+   env RES=gx3 GRID=${IDM}x${JDM} SITE=$SITE MACROID=$MACROID ./comp_ice.esmf
    res=$?
    if [ $res -ne 0 ] ; then 
       echo
--- docs/compilation.md
+++ docs/compilation.md
@@ -17,9 +17,7 @@ See the [HPC environment](installation.md#hpc-environment) section for details.
 ::::
 
 :::{note}
-Olivia support is a work in progress. The status of each step is called out below: the
-MSCPROGS build is the most complete, while HYCOM-CICE is untested and `hycom_ALL` is
-incomplete. These notes track [README.olivia](https://github.com/kvikende/NERSC-HYCOM-CICE/blob/wip-olivia/README.olivia).
+Olivia support is a work in progress. MSCPROGS compilation is tested; HYCOM-CICE and `hycom_ALL` are not yet covered.
 :::
 
 ## Compile MSCPROGS (libhycnersc.a)
@@ -114,11 +112,6 @@ After installation, `libfabm.a` is at:
 ${HOME}/local/fabm/hycom/lib64/libfabm.a
 ```
 
-:::{caution}
-On Olivia, BGC/FABM compilation is **untested** and is not covered by [README.olivia](https://github.com/kvikende/NERSC-HYCOM-CICE/blob/wip-olivia/README.olivia). In
-principle it would use `-DCMAKE_Fortran_COMPILER=ifx` in place of `ifort` above.
-:::
-
 ## Compile HYCOM-CICE
 
 > **Before compiling:** source the HPC environment file to load the correct modules and compilers
@@ -174,45 +167,6 @@ EXPT_ID=<EXPT_ID>         # e.g. 01.0
 cd ${WORK}/${CONFIGNAME}/expt_${EXPT_ID}
 bash ${HOME}/NERSC-HYCOM-CICE/bin/compile_model.sh ifort -u
 ```
-
-::::{dropdown} Olivia (NRIS/Sigma2)
-
-:::{caution}
-The Olivia HYCOM-CICE build is **untested**. The build files
-(`Linux.olivia.ifx_cice.V22`/`.V23` and `Macros.Linux.olivia.ifx`) are in place, but the
-full compile and the downstream `create_ref_case.sh` setup step have not been verified end
-to end (see the known issues below).
-:::
-
-Point the config symlink at the Olivia build files (choose `V22` or `V23`):
-
-```bash
-ln -sf Linux.olivia.ifx_cice.V23 \
-    ${HOME}/NERSC-HYCOM-CICE/hycom/RELO/config/Linux.olivia.ifx_cice
-```
-
-Compile, selecting the site explicitly with `-c olivia` (hostname/domain autodetection does
-not work on Olivia) and the `ifx` compiler:
-
-```bash
-CONFIGNAME=<CONFIGNAME>   # e.g. TP2a0.10
-EXPT_ID=<EXPT_ID>         # e.g. 01.0
-
-cd ${WORK}/${CONFIGNAME}/expt_${EXPT_ID}
-bash ${HOME}/NERSC-HYCOM-CICE/bin/compile_model.sh -c olivia -u ifx
-```
-
-`-c olivia` makes the script use the `Linux.olivia.ifx*` macros and resolve ESMF from
-`$EBROOTESMF`; for CICE it passes `CC=icx`.
-
-:::{note}
-**Known unresolved issues** (from [README.olivia](https://github.com/kvikende/NERSC-HYCOM-CICE/blob/wip-olivia/README.olivia): `create_ref_case.sh` has a `module load`
-at line 132, and `bin/ice_climatology/extract_clim_iceh.sh` has `module load` lines (87 and
-90) whose Olivia-equivalent modules are not compatible with the compilation environment.
-These need adjusting before the setup/run steps will work.
-:::
-
-::::
 
 The script manages the `build/` directory containing a per-experiment copy of the source:
 
@@ -321,18 +275,3 @@ csh ./Make_clean.com
 csh ./Make_all.com
 ```
 
-::::{dropdown} Olivia (NRIS/Sigma2)
-
-:::{caution}
-The Olivia `hycom_ALL` build is **not complete**. Set the compiler to `intelIFX` in
-`Make_all.src`:
-
-```
-setenv ARCH intelIFX
-```
-
-then run `csh ./Make_all.com`. Per [README.olivia](https://github.com/kvikende/NERSC-HYCOM-CICE/blob/wip-olivia/README.olivia), only the NetCDF routines build correctly
-so far (`csh Make_ncdf.com`); the remaining utilities are not yet ported.
-:::
-
-::::
